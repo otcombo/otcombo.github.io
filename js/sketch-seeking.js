c82934b02fce18dd7374574af1609b286c2a49f6
Han Li <otcombo@gmail.com>
2020-02-11 16:07:51 +0800

    js update

--- js/sketch-seeking.js
+++ js/sketch-seeking.js
@@ -32,6 +32,7 @@ function draw() {
   translate(width / 2, height * 0.3);
 
   for (var i = 0; i < 180; i += 3) {
+
     var w = 2 * r * pow(2, sin(time + i)) + cos(time + i) * sin(time + i) / tan(time + i);
     var x = r * noise(xoff, i / 200) * pow(2, cos(i)) + sin(i);
     var y = r * noise(yoff, i / 200) * pow(2, sin(i)) * cos(i);
